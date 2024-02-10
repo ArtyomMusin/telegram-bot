c33f1cd92eac1b5727741116ae41f71ddedc1e4a
Artyom Musin <atm759@mail.ru>
2024-02-10 21:42:34 +0300

    game has been finished

---
+++ game.js
@@ -0,0 +1,82 @@
+module.exports = class Game {
+    constructor(bot, chatId) {
+        this.chatId = chatId
+        this.bot = bot
+        this.matches = {}
+        this.gameOptions = this._getGameOptions()
+        this.count = 0
+        this.allTry = 0
+    }
+
+    _getGameOptions () {
+        return {
+            reply_markup: JSON.stringify({
+                inline_keyboard: [
+                    [{text: '7', callback_data: '7'}, {text: '8', callback_data: '8'}, {text: '9', callback_data: '9'}],
+                    [{text: '4', callback_data: '4'}, {text: '5', callback_data: '5'}, {text: '6', callback_data: '6'}],
+                    [{text: '1', callback_data: '1'}, {text: '2', callback_data: '2'}, {text: '3', callback_data: '3'}],
+                    [{text: '0', callback_data: '0'}]
+                ]
+            })
+        }
+    }
+
+    _addMatch (value) {
+        this.matches[this.chatId] = {
+            value,
+            finished: false
+        }
+    }
+
+    _incrementCount () {
+        this.count += 1
+    }
+
+    _incrementTry () {
+        this.allTry += 1
+    }
+
+    async startGame () {
+        this._incrementTry()
+        const randomNumber = Math.floor(Math.random() * 10)
+        this._addMatch(randomNumber)
+        await this.bot.sendMessage(this.chatId, 'I made a number from 0 to 9, what is this number?', this.gameOptions)
+    }
+
+    async checkResult (data) {
+        if (data === '/again') {
+            this.startGame(this.chatId)
+            return true
+        }
+
+        if (data === '/stop') {
+            this._finishGame()
+            return false
+        }
+
+        if (Number(data) === this.matches[this.chatId]?.value) {
+            this._incrementCount()
+            await this.bot.sendMessage(this.chatId, `You guessed it! This is a figure ${this.matches[this.chatId]?.value}`)
+        } else {
+            await this.bot.sendMessage(this.chatId, `Unfortunately, you made a mistake! It was a figure ${this.matches[this.chatId]?.value}`)
+        }
+        this._gameContinue()
+        return true
+    }
+
+    async _gameContinue () {
+        const options = {
+            reply_markup: JSON.stringify({
+                inline_keyboard: [
+                    [{text: 'Да', callback_data: '/again'}],
+                    [{text: 'Нет', callback_data: '/stop'}]
+                ]
+            })
+        }
+        this.bot.sendMessage(this.chatId, 'Do you want to try again?', options)
+    }
+
+    async _finishGame () {
+        this.bot.sendMessage(this.chatId, `The game is over. You have guessed ${this.count} numbers out of ${this.allTry}`)
+    }
+}
--- index.js
+++ index.js
@@ -1,42 +1,45 @@
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api')
+const Game = require('./game')
 
 const token = '6571139248:AAEBIwcYmKuYvhYVfowzsSL3WmYu6QWB6rU'
 
 const bot = new TelegramBot(token, {polling: true});
 
 const commands = [
-    {command: '/start', description: 'Старт программы'},
-    {command: '/name', description: 'Узнать имя разработчика'},
-    {command: '/info', description: 'Узнать доступные команды'},
-    {command: '/game', description: 'Сыграть в мини-игру'},
+    {command: '/info', description: 'Find out the available commands'},
+    {command: '/game', description: 'Play a mini-game'},
 ]
 
 bot.setMyCommands(commands)
 
-const chats = {}
-const gameOptions = {
-
-}
+let game = null
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     switch (msg.text) {
         case '/start':
-            bot.sendMessage(chatId, 'Привет, мой новый друг')
-            break
-        case '/name':
-            bot.sendMessage(chatId, 'Имя разработчика - Артем')
+            await bot.sendMessage(chatId, 'Hello, my new friend')
+            bot.sendMessage(chatId, commands.map(command => `${command.command} - ${command.description}`).join(" \n"))
             break
         case '/game':
-            await bot.sendMessage(chatId, 'Сейчас я загадаю число от 0 до 9, а Вы отгадайте')
-            const randomNumber = Math.random() * 10
-            chats[chatId] = randomNumber
-            await bot.sendMessage(chatId, 'Отгадывайте')
+            game = new Game(bot, chatId)
+            game.startGame()
             break
         case '/info':
             bot.sendMessage(chatId, commands.map(command => `${command.command} - ${command.description}`).join(" \n"))
             break
         default:
-            bot.sendMessage(chatId, 'Для просмотра доступных команд напишите "/info"')
+            bot.sendMessage(chatId, 'To view the available commands, write "/info"')
+    }
+})
+
+bot.on('callback_query',async msg => {
+    const { data } = msg
+
+    if (game) {
+        const result = await game.checkResult(data)
+        if (!result) {
+            game = null
+        }
     }
 })
